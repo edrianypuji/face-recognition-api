0442800a8bb52dbaebac3325104b8f811a04b48b
Edriany <18370317+edrianypuji@users.noreply.github.com>
2019-07-24 06:56:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ app.use(cors());
 
 
 
-app.get('/',(req,res) => {
-  res.send(database.users);
+app.get('/',(req,res) => {res.send('it is working!');
 })
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
